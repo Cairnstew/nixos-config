3f4384cb398fe6a53297a2f43f08e8ccb1a71619
seancairns14 <sean.cairnsst@gmail.com>
2025-05-16 14:54:51 -0500

    Update default.nix

--- configurations/nixos/server/default.nix
+++ configurations/nixos/server/default.nix
@@ -1,4 +1,4 @@
-{ flake, pkgs, config, ... }:
+{ flake, pkgs, ... }:
 
 let
   inherit (flake) inputs;
@@ -7,38 +7,7 @@ in
 {
   imports = [
     self.nixosModules.default
-    #inputs.nixos-hardware.nixosModules.lenovo-thinkpad-p14s-amd-gen4
-
     ./configuration.nix
   ];
-
-  boot.initrd.kernelModules = [ "nvidia" ];
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-
-  hardware.nvidia-container-toolkit.enable = true;
-
-  hardware.opengl = {
-    enable = true;
-    #driSupport = true;
-    driSupport32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false; # Leave as false unless you're testing the open-source modules
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-
-  nixpkgs.config.allowUnfree = true;
   
-  #services.tailscale.enable = true;
-  environment.systemPackages = with pkgs; [
-    micro
-    zed-editor
-    calibre
-  ];
 }
