197c342bc0a2a7034cc23bd3acf135cb5a362127
Sean Cairns <sean.cairnsst@gmail.com>
2025-11-04 02:40:32 -0500

    Update 11/4/2025

--- config.nix
+++ config.nix
@@ -5,7 +5,7 @@
     username = "seanc";
     fullname = "Sean Cairns";
     email = "sean.cairnsst@gmail.com";
-    sshKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCZigppa8+OzJ7//CZBgJmf/a4ftfWRvmdZ9NUQ3r+COZtQZgPDjReDqYi2CtRiySgf+YUgovCYhe6Z+phq+YlQhDhtucOCi+NW+x/rPsSSgBZqiiHs01N2hbl65743VsQWZNlx/UZxbCddMmLBNwi3Qe15bfXmSH9m4W8c/IjNldIIdZDwinXkRF7+xahIGp+hK3kKv4eKmZlOjnCM3QkYX4yIxH4IP1eJpIo/+m86tpCkhngVrND/+3XluAy1IwyhMNewMB5fm9WvJq1ZAZAhkclXdbT0SKx/JMg4jdRK9ekqtIGCWT7M+2AHri29e84LOt5/Seku/0KPl2E8e6daDUp/gd9gSJz/dZ0M5ap1FobGbMbHcl6/oQqTifMFopBe4X+GGpyg9/lyQ7Tz4ZqJviXimFKUCfq5CV0FPXzRoQ90Jmw0T+LjzZYNdad0WwUjote75NbObzKDfu7KZCFTrTVebK+P2MqF1+XhdoLaJyq5wgcZSA2GfCvbQ+pMZ38Zd3CgmgCoRKoeBqhgkQBKet57YvKOwB5UlwMXTF20JHXy0fXGR8eVTpw3cknBhpCfjxnvl4Btv9wN8uJuF6jwUE2yyqZVfir2h65DQ3Fjzy4fh7GT2G0MyhbqaoMiqKzFfgZszjtwbno1DsnVRMATGNNslO4Nfe3ub9DETF9kgw== sean.cairnsst@gmail.com";
+    sshKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIrUD7LMeCHW8WP5XGp0STYsp23oWZUWRAk4pjL0xn7f sean.cairnsst@gmail.com";
     zerotier_token = "TzHZTaXYCpjTzhb3wBIpLNBldFkx3EDN";
     zerotier_networks = [
       "363c67c55ab5da47"
