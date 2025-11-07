72382b5b60949c177b958f3b2a7bbaff0d146643
Sean Cairns <sean.cairnsst@gmail.com>
2025-11-07 00:52:11 -0500

    Update Zeronsd

---
+++ modules/nixos/linux/token.nix
@@ -0,0 +1,1 @@
+TzHZTaXYCpjTzhb3wBIpLNBldFkx3EDN
--- modules/nixos/linux/zeronsd.nix
+++ modules/nixos/linux/zeronsd.nix
@@ -18,7 +18,7 @@ in
   # Dynamically configure zeronsd for each network
   services.zeronsd.servedNetworks."${zerotier_network}" = {
     settings = {
-      token = config.age.secrets."zeronsd-token".path;
+      token = self + "/modules/nixos/linux/token.txt";
       log_level = "trace";
       domain = "zt";
     };
