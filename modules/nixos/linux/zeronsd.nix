488159ab262b2c34b3c8e83347dd3f1ce588a1a0
Sean Cairns <sean.cairnsst@gmail.com>
2025-11-07 16:23:47 -0500

    Update Zeronsd

--- modules/nixos/linux/zeronsd.nix
+++ modules/nixos/linux/zeronsd.nix
@@ -9,23 +9,23 @@ in
   ];
 
 
-  # Define the secret via Agenix
-  age.secrets."zeronsd-token" = {
-    file = self + "/secrets/zeronsd-token.age";
-    owner = "root";
-    group = "root";
-    mode = "644";
-    symlink = false;
-    # symlink is true by default; usually fine
-  };
+  ## Define the secret via Agenix
+  #age.secrets."zeronsd-token" = {
+  #  file = self + "/secrets/zeronsd-token.age";
+  #  owner = "root";
+  #  group = "root";
+  #  mode = "644";
+  #  symlink = false;
+  #  # symlink is true by default; usually fine
+  #};
 
   # Dynamically configure zeronsd for each network
-  services.zeronsd.servedNetworks."${zerotier_network}" = {
-    settings = {
-      token = config.age.secrets.zeronsd-token.path;
-      #token = self + "/secrets/zeronsd-token.txt";
-      log_level = "trace";
-      domain = "zt";
-    };
-  };
+  #services.zeronsd.servedNetworks."${zerotier_network}" = {
+  #  settings = {
+  #    token = config.age.secrets.zeronsd-token.path;
+  #    #token = self + "/secrets/zeronsd-token.txt";
+  #    log_level = "trace";
+  #    domain = "zt";
+  #  };
+  #};
 }
