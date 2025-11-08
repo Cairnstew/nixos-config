0abfb162c007aa5855cc0d5550792976bcd4c147
Sean Cairns <sean.cairnsst@gmail.com>
2025-11-08 17:53:40 -0500

    Update Zeronsd

--- modules/nixos/linux/zeronsd.nix
+++ modules/nixos/linux/zeronsd.nix
@@ -20,51 +20,11 @@ in
     mode = "770";
   };
 
-  ###### ZeroNSD service ######
-  environment.systemPackages = [ pkgs.zeronsd ];
-
-  # Configuration file for ZeroNSD
-  environment.etc."zeronsd/${zerotier_network}.yaml".text = ''
-    domain: home
-    log_level: info
-    secret: /var/lib/zerotier-one/authtoken.secret
-    token: ${config.age.secrets."zeronsd-token".path}
-    wildcard: true
-  '';
-
-  systemd.services."zeronsd-${zerotier_network}" = {
-    description = "ZeroNSD for ZeroTier network ${zerotier_network}";
-    after = [ "network.target" "zerotierone.service" ];
-    wants = [ "zerotierone.service" ];
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = "${pkgs.zeronsd}/bin/zeronsd start ${zerotier_network} -c /etc/zeronsd/${zerotier_network}.yaml";
-      Restart = "on-failure";
-      RestartSec = "10s";
-      Environment = "ZEROTIER_CENTRAL_TOKEN_FILE=${config.age.secrets."zeronsd-token".path}";
-      StandardOutput = "journal";
-      StandardError = "journal";
-      User = "zeronsd";
-      Group = "zeronsd";
-      RuntimeDirectory = "zeronsd";
-      #AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
+  services.zerotierone-with-dns = {
+    enable = true;
+    networks = {
+      "homenet.zt" = zerotier_network;
+      #"gamenet.zt" = "<ZEROTIER NETWORK ID>";
     };
-
-    preStart = ''
-      if [ ! -f /etc/zeronsd/${zerotier_network}.yaml ]; then
-        echo "Missing config file /etc/zeronsd/${zerotier_network}.yaml"
-        exit 1
-      fi
-    '';
   };
-
-  # Optional: system user for ZeroNSD
-  users.users.zeronsd = {
-    isSystemUser = true;
-    description = "ZeroNSD service user";
-    group = "zeronsd";
-  };
-  users.groups.zeronsd = {};
 }
