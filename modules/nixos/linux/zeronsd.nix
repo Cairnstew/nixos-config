97a47731918b25a9c4f53da5033753c259ae8b69
Sean Cairns <sean.cairnsst@gmail.com>
2025-11-08 18:23:05 -0500

    Update Zeronsd

--- modules/nixos/linux/zeronsd.nix
+++ modules/nixos/linux/zeronsd.nix
@@ -15,17 +15,40 @@ in
   # Define the secret via Agenix
   age.secrets."zeronsd-token" = {
     file = self + "/secrets/zeronsd-token.age";
-    #owner = "zeronsd";
-    #group = "zeronsd";
-    #mode = "770";
+    owner = "zeronsd";
+    group = "zeronsd";
+    mode = "600";
   };
 
-  
-  services.zeronsd.servedNetworks = {
-    zerotier_network = {
-      settings = {
-        token = "/var/lib/zerotier-one/token";
-      };
+  ###### ZeroNSD service ######
+  environment.systemPackages = [ pkgs.zeronsd ];
+
+  systemd.services."zeronsd-${zerotier_network}" = {
+    description = "ZeroNSD for ZeroTier network ${zerotier_network}";
+    after = [ "network.target" "zerotierone.service" ];
+    wants = [ "zerotierone.service" ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.zeronsd}/bin/zeronsd start ${zerotier_network} -t ${config.age.secrets."zeronsd-token".path} -w -v";
+      Restart = "on-failure";
+      RestartSec = "10s";
+      Environment = "ZEROTIER_CENTRAL_TOKEN_FILE=${config.age.secrets."zeronsd-token".path}";
+      StandardOutput = "journal";
+      StandardError = "journal";
+      User = "zeronsd";
+      Group = "zeronsd";
+      RuntimeDirectory = "zeronsd";
+      #AmbientCapabilities = [ "CAP_NET_BIND_SERVICE" ];
     };
   };
+
+  # Optional: system user for ZeroNSD
+  users.users.zeronsd = {
+    isSystemUser = true;
+    description = "ZeroNSD service user";
+    group = "zeronsd";
+  };
+  users.groups.zeronsd = {};
 }
