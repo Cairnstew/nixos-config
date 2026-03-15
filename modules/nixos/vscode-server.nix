e5dc2b146a10217a55539ac085cb5fa1823a2e40
Cairnstew <sean.cairnsst@gmail.com>
2026-03-15 21:09:40 +0000

    chore(modules): Module vscode server fix imports

--- modules/nixos/vscode-server.nix
+++ modules/nixos/vscode-server.nix
@@ -4,12 +4,12 @@ let
   inherit (inputs) self;
 in
 {
-    imports = [ vscode-server.nixosModules.default ];
+    imports = [ flake.inputs.vscode-server.nixosModules.default ];
 
     options.my.services.vscode-server = {
       enable = lib.mkEnableOption "VSCode Server";
     };
-    
+
     config = lib.mkIf config.my.services.vscode-server.enable {
       services.vscode-server.enable = true;
     };
