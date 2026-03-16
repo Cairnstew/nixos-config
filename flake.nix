539b3123ea4e09b89f07d1c2e18c350c7c09ec04
Cairnstew <sean.cairnsst@gmail.com>
2026-03-16 01:41:58 +0000

    chore(flake): remove dup vscode server input

--- flake.nix
+++ flake.nix
@@ -28,7 +28,6 @@
 
       # Software inputs
       github-nix-ci.url = "github:juspay/github-nix-ci";
-      nixos-vscode-server.url = "github:nix-community/nixos-vscode-server";
       nixos-vscode-server.flake = false;
       nixos-vscode-server.url = "github:nix-community/nixos-vscode-server";
       flake-utils.url = "github:numtide/flake-utils";
