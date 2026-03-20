f2f62ded160819c5c01ef6ae44c1f3151ea552c7
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-20 02:42:23 +0000

    chore(config): Add laptop key to gh secret

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,7 +2,7 @@ let
   config = import ../config.nix;
   users = [ config.me.sshKey ];
 
-  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOQ4JIoLFjDXNB2jtI3K41JzB5HCWk3aigRcXvtzJ7kO root@laptop";
+  laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETE96NnwPAZ0n5y6XcCzoErkrAhulUht/Hho0V829Qy root@laptop";
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJXLC3S2pEuIchrWMtmWiTaJOA+U02HVyRczRNbRjMX root@nixos";
 
   systems = [ laptop server ];
