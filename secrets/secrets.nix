7f87dce7c9094568c06c4dca27c7dde90be14b88
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-20 18:12:56 -0500

    chore(secrets): Fix secret keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -6,10 +6,10 @@ let
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJXLC3S2pEuIchrWMtmWiTaJOA+U02HVyRczRNbRjMX root@nixos";
   wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2wdUAkZEY99Ya//2NcEY71ft3PaHoCQYOr2jIZf/99 root@nixos";
 
-  systems = [ laptop server ];
+  systems = [ laptop server wsl ];
 in
 {
-  "zeronsd-token.age".publicKeys = [ systems wsl ];
+  "zeronsd-token.age".publicKeys = [ systems ];
   "zeronsd-token.age".armor = true;
   "spotify-cred.age".publicKeys = users ++ systems;
   "github-token.age".publicKeys = users ++ systems;
