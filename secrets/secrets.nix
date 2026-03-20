cf5093d7ef59278d122f86b8ab232c0b7fbc7a5c
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-20 17:58:01 -0500

    feat(config): Update secrets include wsl

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,11 +4,12 @@ let
 
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETE96NnwPAZ0n5y6XcCzoErkrAhulUht/Hho0V829Qy root@laptop";
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJXLC3S2pEuIchrWMtmWiTaJOA+U02HVyRczRNbRjMX root@nixos";
+  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2wdUAkZEY99Ya//2NcEY71ft3PaHoCQYOr2jIZf/99 root@nixos";
 
   systems = [ laptop server ];
 in
 {
-  "zeronsd-token.age".publicKeys = [ systems ];
+  "zeronsd-token.age".publicKeys = [ systems wsl ];
   "zeronsd-token.age".armor = true;
   "spotify-cred.age".publicKeys = users ++ systems;
   "github-token.age".publicKeys = users ++ systems;
