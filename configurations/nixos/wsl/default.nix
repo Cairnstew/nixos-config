23da380d4318405665351ba19e5b74454e50874b
Cairnstew <sean.cairnsst@gmail.com>
2026-03-20 22:23:10 +0000

    feat(configuration): Add wsl config

---
+++ configurations/nixos/wsl/default.nix
@@ -0,0 +1,86 @@
+{ config, flake, pkgs, lib, ... }:
+
+let
+  # ── Short aliases ───────────────────────────────────────
+  me   = flake.config.me;
+  user = me.username;
+  self = flake.inputs.self;
+in
+{
+  # ── Imports ────────────────────────────────────────────
+  imports = [
+    ./configuration.nix
+    self.nixosModules.default
+    flake.inputs.nixos-wsl.nixosModules.default
+  ];
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+
+  # ── Hardware configuration ─────────────────────────────
+  hardwareProfiles.gpu.amd = {
+    enable = true;
+  };
+
+  # ── System settings ────────────────────────────────────
+  my.system = {
+    location = {
+      enable    = true;
+      timeZone  = "GB";
+      latitude  = 55.8617;
+      longitude = 4.2583;
+    };
+    battery = {
+      enable = false;
+    };
+  };
+
+  # ── System programs ────────────────────────────────────
+  my.programs = {
+    #spotify.enable = true;
+  };
+
+  # ── System tools ───────────────────────────────────────
+  my.tools = {
+    uup-converter.enable = false;
+  };  
+
+  # Virtualisation
+  my.virtualisation = {
+    waydroid = {
+      enable = false;
+    };
+    docker = {
+      enable = true;
+      users = [ user ];
+    };
+  };
+
+  # ── System services ────────────────────────────────────
+  my.services = {
+    zerotier = {
+      enable   = true;
+      networks = [ me.zerotier_network ];
+      mtu = 1280;
+    };
+
+    # zeronsd = {
+    #   enable         = true;
+    #   zerotierNetwork = me.zerotier_network;
+    #   tokenFile      = config.age.secrets."zeronsd-token".path;
+    # };
+  };
+
+  environment.systemPackages = [
+    self.packages.${pkgs.stdenv.hostPlatform.system}.get-template
+  ];
+
+  # ── Home Manager configuration ─────────────────────────
+  home-manager.users.${user} = {
+
+    my = {
+      programs = {
+
+      };
+    };
+  };
+}
