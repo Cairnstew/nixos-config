bf263b039f91ad4d58ba5b112aaaada7f48c8aa7
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-21 15:51:05 -0500

    chore(secrets): Fix wsl ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -4,7 +4,7 @@ let
 
   laptop = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETE96NnwPAZ0n5y6XcCzoErkrAhulUht/Hho0V829Qy root@laptop";
   server = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINJXLC3S2pEuIchrWMtmWiTaJOA+U02HVyRczRNbRjMX root@nixos";
-  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA2wdUAkZEY99Ya//2NcEY71ft3PaHoCQYOr2jIZf/99 root@nixos";
+  wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIW4gfFRLs6zBtHtWYSrkJ4oCSZ656CAt8oC/CAMVpCN root@DESKTOP-DLSTFLT";
 
   systems = [ laptop server wsl ];
 in
