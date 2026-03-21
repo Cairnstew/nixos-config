a0f560b13359af3e87f9d60d35e90f736c6e823f
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-21 16:45:58 -0500

    feat(secrets): Update reencrypt script to sign in

--- secrets/reencrypt.bash
+++ secrets/reencrypt.bash
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Add account if none configured
+if ! op account list &>/dev/null || [ -z "$(op account list)" ]; then
+    echo "No 1Password accounts configured, adding one..."
+    op account add
+fi
+
 # Sign in if not already
 if ! op account get &>/dev/null; then
     eval $(op signin)
