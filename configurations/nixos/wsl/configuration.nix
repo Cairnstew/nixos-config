f229c9b5c3cbe6009d36a24772ae59aa73baf351
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-22 08:51:52 -0500

    chore(configuration): Add dig for testing

--- configurations/nixos/wsl/configuration.nix
+++ configurations/nixos/wsl/configuration.nix
@@ -12,6 +12,10 @@
   wsl.enable = true;
   networking.hostName = "wsl";
 
+  environment.systemPackages = [
+    pkgs.dig
+  ];
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
