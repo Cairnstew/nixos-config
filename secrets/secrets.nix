149d2e66943ac93de765c1f9a2cc560d37d996c5
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-23 09:25:25 -0500

    feat(secrets): Migrate to tailscale

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -7,14 +7,21 @@ let
   wsl = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIW4gfFRLs6zBtHtWYSrkJ4oCSZ656CAt8oC/CAMVpCN root@DESKTOP-DLSTFLT";
 
   systems = [ laptop server wsl ];
+  all = users ++ systems;
 in
 {
-  "zeronsd-token.age".publicKeys = users ++ systems;
-  "spotify-cred.age".publicKeys = users ++ systems;
-  "github-token.age".publicKeys = users ++ systems;
-  "obsidian-git-token.age".publicKeys = users ++ systems;
-  "nixos-config-git-token.age".publicKeys = users ++ systems;
-  "nixos-config-cache-token.age".publicKeys = users ++ systems;
+  "zeronsd-token.age".publicKeys = all;
+  "spotify-cred.age".publicKeys = all;
+  "github-token.age".publicKeys = all;
+  "obsidian-git-token.age".publicKeys = all;
+  "nixos-config-git-token.age".publicKeys = all;
+  "nixos-config-cache-token.age".publicKeys = all;
+  "zt-ssh-key.age".publicKeys = all;
+
+  # Tailscale Keys - Expire after 90 Days - Created : 3/23/2026
+  "tailscale-authkey.age".publicKeys = all;
+  "tailscale-apikey.age".publicKeys = all;
+
+  # OnePass Key
   "onepassword-token.age".publicKeys = users;
-  "zt-ssh-key.age".publicKeys = users ++ systems;
 }
