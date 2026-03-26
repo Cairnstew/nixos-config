d21e6200062270d43ee3b34ebbb3cea5de8abf0c
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-26 17:36:42 -0500

    feat(modules): Merge _1pass modules

--- modules/nixos/_1password/default.nix
+++ modules/nixos/_1password/default.nix
@@ -2,24 +2,17 @@
 let
   inherit (flake) config inputs;
   inherit (inputs) self;
-  
 in
 {
-
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
-    # Certain features, including CLI integration and system authentication support,
-    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
     polkitPolicyOwners = [ "${flake.config.me.username}" ];
   };
   security.polkit.enable = true;
 
-
-  home-manager.users.${config.me.username}.my.programs = { 
-    ssh-1password.enable = true;
+  home-manager.users.${config.me.username} = {
+    imports = [ ./home.nix ];
+    my.programs.ssh-1password.enable = true;
   };
-
-
-
 }
