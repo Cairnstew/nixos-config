22addc2a5f4ecd946499537b317b7c1670e6eba6
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-27 11:50:33 +0000

    chore(modules): fix nvidia cache key

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -177,7 +177,7 @@ in
         ];
 
         trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= cuda-maintainers.cachix.org-1:0dqXKq+exampleReplaceThisKey= cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         ];
       };
     })
