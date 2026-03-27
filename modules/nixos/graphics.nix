9fb690eb36fc28a36a1e4d6164313f68ab16e96e
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-27 12:33:19 +0000

    feat(modules): Fix nvidia cache appending

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -171,13 +171,10 @@ in
     })
     (lib.mkIf nvidia.CUDABinaryCache {
       nix.settings = {
-        substituters = [
-          "https://cache.nixos.org/"
+        extra-substituters = [
           "https://cuda-maintainers.cachix.org"
         ];
-
-        trusted-public-keys = [
-          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        extra-trusted-public-keys = [
           "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         ];
       };
