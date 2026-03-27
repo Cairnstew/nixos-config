893042c0f920d38ee3af111dea7c43bb86862d13
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-27 12:26:26 +0000

    feat(modules): Update nvidia cache

--- modules/nixos/graphics.nix
+++ modules/nixos/graphics.nix
@@ -177,6 +177,7 @@ in
         ];
 
         trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
           "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         ];
       };
