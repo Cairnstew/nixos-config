2c4375bd6381b3f2777551537aadff7caa7c0223
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-30 21:26:53 -0500

    feat(configuration): Add screen pkg to server config

--- configurations/nixos/server/default.nix
+++ configurations/nixos/server/default.nix
@@ -69,6 +69,7 @@ in
 
   environment.systemPackages = [
     self.packages.${pkgs.stdenv.hostPlatform.system}.get-template
+    pkgs.screen
   ];
 
   # ── Home Manager configuration ─────────────────────────
