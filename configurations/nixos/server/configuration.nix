46fce0a5a8b4c4fa6fa5b9e76c24c4c1aa40f44b
Sean Cairns <sean.cairnsst@gmail.com>
2026-03-31 19:49:20 -0500

    feat(configuration): Remove unfree declaration

--- configurations/nixos/server/configuration.nix
+++ configurations/nixos/server/configuration.nix
@@ -56,9 +56,6 @@
     packages = with pkgs; [];
   };
 
-  nixpkgs.config = {
-      allowUnfree = true;
-    };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
