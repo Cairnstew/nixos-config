f740d4b9f3a2a8ae5e9ab0201f6d075d4b40f9e9
Sean Cairns <sean.cairnsst@gmail.com>
2026-04-03 08:16:20 -0500

    feat(terraform): Update flake part

--- modules/flake-parts/terraform.nix
+++ modules/flake-parts/terraform.nix
@@ -1,44 +1,39 @@
 { inputs, lib, ... }:
 {
-  perSystem = { pkgs, system, ... }:
-  let
-    # Filter nixosConfigurations to only cloud hosts
-    cloudHosts = lib.filterAttrs
-      (_: cfg: cfg.config.my.cloud-vm.enable)
-      inputs.self.nixosConfigurations;
-
-    awsHosts = lib.filterAttrs
-      (_: cfg: cfg.config.my.cloud-vm.provider == "aws")
-      cloudHosts;
-
-    # Build the -var=cloud_hosts=... JSON for a given set of hosts
-    hostsVar = hosts: builtins.toJSON (lib.mapAttrs (_: _: {
+  flake.cloudHosts = {
+    aws-webserver = {
       instance_type = "t3.micro";
       nixos_release = "24.11";
-    }) hosts);
-
-    mkTfApp = hosts: pkgs.writeShellScript "tf" ''
-      set -e
-      cd ${toString ./../../terraform}
-      exec ${pkgs.opentofu}/bin/tofu \
-        -var="flake_root=${toString ./../../.}" \
-        -var='cloud_hosts=${hostsVar hosts}' \
-        "$@"
-    '';
-  in {
-    # nix run .#tf -- apply  (all aws hosts)
-    apps.tf = {
-      type = "app";
-      program = toString (mkTfApp awsHosts);
     };
+    aws-bastion = {
+      instance_type = "t3.micro";
+      nixos_release = "24.11";
+    };
+  };
 
-    # nix run .#tf-aws-webserver -- apply  (single host)
-    apps = lib.mapAttrs' (name: _: {
+  perSystem = { pkgs, ... }:
+  let
+    awsHosts = inputs.self.cloudHosts;
+
+    hostsVar = hosts: builtins.toJSON hosts;
+
+    mkTfApp = hosts: {
+      type = "app";
+      program = toString (pkgs.writeShellScript "tf" ''
+        set -e
+        cd ${toString ./../../terraform}
+        exec ${pkgs.opentofu}/bin/tofu \
+          -var="flake_root=${toString ./../../.}" \
+          -var='cloud_hosts=${hostsVar hosts}' \
+          "$@"
+      '');
+    };
+  in {
+    apps = {
+      tf = mkTfApp awsHosts;
+    } // lib.mapAttrs' (name: _: {
       name = "tf-${name}";
-      value = {
-        type = "app";
-        program = toString (mkTfApp { ${name} = awsHosts.${name}; });
-      };
+      value = mkTfApp { ${name} = awsHosts.${name}; };
     }) awsHosts;
 
     devShells.terraform = pkgs.mkShell {
