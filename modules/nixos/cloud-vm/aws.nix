18ac52aafad73068ed46585766fe48692eaa783e
Sean Cairns <sean.cairnsst@gmail.com>
2026-04-03 18:13:10 -0500

    feat(terraform): Update terraform config

--- modules/nixos/cloud-vm/aws.nix
+++ modules/nixos/cloud-vm/aws.nix
@@ -1,11 +1,14 @@
-{ config, lib, modulesPath, ... }:
+{ flake, config, lib, modulesPath, ... }:
 
 let
   cfg = config.my.cloud-vm;
 in {
-  config = lib.mkIf (cfg.enable && cfg.provider == "aws") {
-    imports = [
-      "${modulesPath}/virtualisation/amazon-image.nix"
-    ];
+  imports = [
+    flake.inputs.self.nixosModules.secrets
+    "${modulesPath}/virtualisation/amazon-image.nix"
+  ];
+
+  config = lib.mkIf cfg.enable {
+    my.secrets.enable = true;
   };
 }
