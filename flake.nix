473df1838a23543f1e9e1f04dfd0a2502eec22ce
Sean Cairns <sean.cairnsst@gmail.com>
2026-04-03 10:59:31 -0500

    feat(configuration): Add base cache to flake

--- flake.nix
+++ flake.nix
@@ -3,11 +3,13 @@
 
   nixConfig = {
     substituters = [
+      "https://cache.nixos.org"          # ← missing!
       "https://cairnstew-nixos-config-cache.cachix.org"
       "https://cuda-maintainers.cachix.org"
       "https://nix-community.cachix.org"
     ];
     trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="   # ← missing!
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
       "cairnstew-nixos-config-cache.cachix.org-1:1150paajFeK18p7Eie/4L8iews3pbFbVp3eOxkmXar4="
