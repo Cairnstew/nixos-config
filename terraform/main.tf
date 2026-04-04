b144dcef5f892f9076aac58006a5e160d5790362
Sean Cairns <sean.cairnsst@gmail.com>
2026-04-04 08:06:51 -0500

    feat(configuration): Update terraform config

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,11 @@
 locals {
-  hosts = jsondecode(var.cloud_hosts)
-
+  hosts     = jsondecode(var.cloud_hosts)
   aws_hosts = { for k, v in local.hosts : k => v if v.provider == "aws" }
+}
+
+# One provider per unique region used by aws hosts
+provider "aws" {
+  region = "eu-west-1"  # or make dynamic if you have multiple regions
 }
 
 module "aws" {
@@ -12,4 +16,8 @@ module "aws" {
   instance_type = each.value.instance_type
   nixos_release = each.value.nixos_release
   region        = each.value.region
+
+  providers = {
+    aws = aws
+  }
 }
