fce2d4c64ad699abbaa83ae02d1dcd6deaf168c6
Sean Cairns <sean.cairnsst@gmail.com>
2026-04-04 05:59:58 -0500

    feat(modules): Update aws.nix

--- modules/nixos/cloud-vm/aws.nix
+++ modules/nixos/cloud-vm/aws.nix
@@ -4,11 +4,11 @@ let
   cfg = config.my.cloud-vm;
 in {
   imports = [
-    flake.inputs.self.nixosModules.secrets
+    #flake.inputs.self.nixosModules.secrets
     "${modulesPath}/virtualisation/amazon-image.nix"
   ];
 
-  config = lib.mkIf cfg.enable {
-    my.secrets.enable = true;
-  };
+  #onfig = lib.mkIf cfg.enable {
+  #  my.secrets.enable = true;
+  #};
 }
