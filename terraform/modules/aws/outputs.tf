7848ee08d3a6246751a6f53622cb23a90dcd7458
Sean Cairns <sean.cairnsst@gmail.com>
2026-04-04 06:00:13 -0500

    feat(terraform): Modularize terraform config

---
+++ terraform/modules/aws/outputs.tf
@@ -0,0 +1,11 @@
+output "public_ip" {
+  value = aws_eip.this.public_ip
+}
+
+output "instance_id" {
+  value = aws_instance.this.id
+}
+
+output "ami_id" {
+  value = module.nixos_image.ami
+}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,27 +1,6 @@
-output "instances" {
-  description = "Deployed EC2 instances"
+output "aws_hosts" {
+  description = "Public IPs of all AWS hosts"
   value = {
-    for name, instance in aws_instance.cloud_hosts :
-    name => {
-      id            = instance.id
-      public_ip     = instance.public_ip
-      private_ip    = instance.private_ip
-      instance_type = instance.instance_type
-      ami           = instance.ami
-    }
+    for k, mod in module.aws : k => mod.public_ip
   }
-}
-
-output "ssh_command" {
-  description = "SSH command to connect to instances"
-  value = {
-    for name, instance in aws_instance.cloud_hosts :
-    name => "ssh -i id_deployer.pem root@${instance.public_ip}"
-  }
-}
-
-output "ssh_key_path" {
-  description = "Path to the SSH private key"
-  value       = local_sensitive_file.deployer_ssh_key.filename
-  sensitive   = true
 }
