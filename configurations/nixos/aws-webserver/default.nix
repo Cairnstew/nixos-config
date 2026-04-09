8eaa1dbcc32fb5b1f091f3524bd0f11f4938c111
Sean Cairns <sean.cairnsst@gmail.com>
2026-04-09 09:31:15 -0500

    chore(modules): Clean modules

--- configurations/nixos/aws-webserver/default.nix
+++ configurations/nixos/aws-webserver/default.nix
@@ -2,33 +2,12 @@
 
 let
   self = flake.inputs.self;
-in
-{
+in {
   imports = [
-    self.nixosModules.cloud-vm
+    
   ];
 
-  nixpkgs.hostPlatform = "x86_64-linux";  # ← explicit, don't rely on module
-
-  cloud-vm = {
-    enable       = true;
-    provider     = "aws";
-    profile      = "web";
-    instanceType = "t3.small";
-    nixosRelease = "25.11";
-    region       = "eu-west-1";
-    secretsPath  = "/run/agenix/aws-cloud";
-    diskDevice   = "/dev/nvme0n1";
-  };
-
-  cloud.aws.hosts = {
-    my-server = {
-      instance_type  = "t3.micro";
-      region         = "eu-west-1";
-      nixos_release  = "25.11";
-      ssh_public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAA... user@host";
-    };
-  };
+  nixpkgs.hostPlatform = "x86_64-linux";
 
   services.openssh.settings.PermitRootLogin = "prohibit-password";
 
