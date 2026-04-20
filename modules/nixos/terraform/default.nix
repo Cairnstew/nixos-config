35ea3ec8fd4edbb0048357e8dbeca6e5e2615cbd
Sean Cairns <sean.cairnsst@gmail.com>
2026-04-20 12:43:11 -0500

    feat(modules): Update terraform

--- modules/nixos/terraform/default.nix
+++ modules/nixos/terraform/default.nix
@@ -3,10 +3,287 @@
 
 {
   imports = [
-    ./providers.nix
-    ./variables.nix
     ./vpc.nix
-    ./ec2.nix
-    ./outputs.nix
+    ./ecs.nix
   ];
+
+  config = { config, lib, ... }:
+    let
+      inherit (lib)
+        optionalAttrs foldl' recursiveUpdate
+        mapAttrs mapAttrsToList attrValues imap0;
+
+      cfg = config.services.ecsFargate;
+      rn  = n: "fargate_${n}";
+
+      containerResources = mapAttrs (name: c:
+        let r = rn name; in
+        foldl' recursiveUpdate {} [
+
+          { resource.aws_cloudwatch_log_group.${r} = {
+              name              = "/ecs/${cfg.clusterName}/${name}";
+              retention_in_days = c.logRetentionDays;
+              tags              = cfg.tags;
+            };
+          }
+
+          { resource.aws_iam_role."${r}_execution" = {
+              name = "${cfg.clusterName}-${name}-execution";
+              assume_role_policy = builtins.toJSON {
+                Version = "2012-10-17";
+                Statement = [{
+                  Effect    = "Allow";
+                  Principal = { Service = "ecs-tasks.amazonaws.com"; };
+                  Action    = "sts:AssumeRole";
+                }];
+              };
+              tags = cfg.tags;
+            };
+            resource.aws_iam_role_policy_attachment."${r}_execution_policy" = {
+              role       = "\${aws_iam_role.${r}_execution.name}";
+              policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy";
+            };
+          }
+
+          (optionalAttrs (c.taskRoleArn == null) (
+            foldl' recursiveUpdate {} ([{
+              resource.aws_iam_role."${r}_task" = {
+                name = "${cfg.clusterName}-${name}-task";
+                assume_role_policy = builtins.toJSON {
+                  Version = "2012-10-17";
+                  Statement = [{
+                    Effect    = "Allow";
+                    Principal = { Service = "ecs-tasks.amazonaws.com"; };
+                    Action    = "sts:AssumeRole";
+                  }];
+                };
+                tags = cfg.tags;
+              };
+            }] ++ (imap0 (i: arn: {
+              resource.aws_iam_role_policy_attachment."${r}_task_extra_${toString i}" = {
+                role       = "\${aws_iam_role.${r}_task.name}";
+                policy_arn = arn;
+              };
+            }) c.extraPolicies))
+          ))
+
+          { resource.aws_ecs_task_definition.${r} = {
+              family                   = "${cfg.clusterName}-${name}";
+              requires_compatibilities = [ "FARGATE" ];
+              network_mode             = "awsvpc";
+              cpu                      = toString c.cpu;
+              memory                   = toString c.memory;
+              execution_role_arn       = "\${aws_iam_role.${r}_execution.arn}";
+              task_role_arn            =
+                if c.taskRoleArn != null then c.taskRoleArn
+                else "\${aws_iam_role.${r}_task.arn}";
+              container_definitions = builtins.toJSON ([
+                ({
+                  name      = name;
+                  image     = c.image;
+                  cpu       = c.cpu;
+                  memory    = c.memory;
+                  essential = true;
+                  logConfiguration = {
+                    logDriver = "awslogs";
+                    options = {
+                      "awslogs-group"         = "/ecs/${cfg.clusterName}/${name}";
+                      "awslogs-region"        = cfg.region;
+                      "awslogs-stream-prefix" = "ecs";
+                    };
+                  };
+                }
+                // optionalAttrs (c.port != null)      { portMappings = [{ containerPort = c.port; protocol = "tcp"; }]; }
+                // optionalAttrs (c.command != null)   { command = c.command; }
+                // optionalAttrs (c.environment != {}) { environment = mapAttrsToList (k: v: { name = k; value = v; }) c.environment; }
+                // optionalAttrs (c.secrets != {})     { secrets = mapAttrsToList (k: v: { name = k; valueFrom = v; }) c.secrets; }
+                )
+              ]);
+              tags = cfg.tags;
+            };
+          }
+
+          { resource.aws_security_group.${r} = {
+              name        = "${cfg.clusterName}-${name}-svc";
+              description = "ECS service SG for ${name}";
+              vpc_id      = cfg.vpc.id;
+              tags        = cfg.tags;
+              egress = [{ from_port = 0; to_port = 0; protocol = "-1"; cidr_blocks = [ "0.0.0.0/0" ]; }];
+            };
+          }
+
+          (optionalAttrs (c.public && c.port != null) (foldl' recursiveUpdate {} [
+            { resource.aws_security_group."${r}_alb" = {
+                name        = "${cfg.clusterName}-${name}-alb";
+                description = "ALB SG for ${name}";
+                vpc_id      = cfg.vpc.id;
+                tags        = cfg.tags;
+                ingress = [{ from_port = 80; to_port = 80; protocol = "tcp"; cidr_blocks = [ "0.0.0.0/0" ]; }];
+                egress  = [{ from_port = 0;  to_port = 0;  protocol = "-1"; cidr_blocks = [ "0.0.0.0/0" ]; }];
+              };
+            }
+            { resource.aws_lb.${r} = {
+                name               = "${cfg.clusterName}-${name}";
+                internal           = false;
+                load_balancer_type = "application";
+                security_groups    = [ "\${aws_security_group.${r}_alb.id}" ];
+                subnets            = cfg.vpc.publicSubnetIds;
+                tags               = cfg.tags;
+              };
+            }
+            { resource.aws_lb_target_group.${r} = {
+                name        = "${cfg.clusterName}-${name}";
+                port        = c.port;
+                protocol    = c.protocol;
+                vpc_id      = cfg.vpc.id;
+                target_type = "ip";
+                tags        = cfg.tags;
+              } // optionalAttrs (c.healthCheck != null) {
+                health_check = {
+                  path                = c.healthCheck.path;
+                  interval            = c.healthCheck.interval;
+                  timeout             = c.healthCheck.timeout;
+                  healthy_threshold   = c.healthCheck.healthy;
+                  unhealthy_threshold = c.healthCheck.unhealthy;
+                  matcher             = c.healthCheck.matcher;
+                };
+              };
+            }
+            { resource.aws_lb_listener.${r} = {
+                load_balancer_arn = "\${aws_lb.${r}.arn}";
+                port              = 80;
+                protocol          = "HTTP";
+                default_action    = [{ type = "forward"; target_group_arn = "\${aws_lb_target_group.${r}.arn}"; }];
+              };
+              resource.aws_security_group_rule."${r}_alb_ingress" = {
+                type                     = "ingress";
+                from_port                = c.port;
+                to_port                  = c.port;
+                protocol                 = "tcp";
+                security_group_id        = "\${aws_security_group.${r}.id}";
+                source_security_group_id = "\${aws_security_group.${r}_alb.id}";
+              };
+            }
+          ]))
+
+          { resource.aws_ecs_service.${r} = {
+              name            = "${cfg.clusterName}-${name}";
+              cluster         = "\${aws_ecs_cluster.${cfg.clusterName}.id}";
+              task_definition = "\${aws_ecs_task_definition.${r}.arn}";
+              desired_count   = c.scaling.min;
+              launch_type     = "FARGATE";
+              network_configuration = [{
+                subnets          = cfg.vpc.privateSubnetIds;
+                security_groups  = [ "\${aws_security_group.${r}.id}" ];
+                assign_public_ip = c.assignPublicIp;
+              }];
+            } // optionalAttrs (c.public && c.port != null) {
+              load_balancer = [{
+                target_group_arn = "\${aws_lb_target_group.${r}.arn}";
+                container_name   = name;
+                container_port   = c.port;
+              }];
+              depends_on = [ "aws_lb_listener.${r}" ];
+            };
+          }
+
+          { resource.aws_appautoscaling_target.${r} = {
+              max_capacity       = c.scaling.max;
+              min_capacity       = c.scaling.min;
+              resource_id        = "service/${cfg.clusterName}/${cfg.clusterName}-${name}";
+              scalable_dimension = "ecs:service:DesiredCount";
+              service_namespace  = "ecs";
+              depends_on         = [ "aws_ecs_service.${r}" ];
+            };
+            resource.aws_appautoscaling_policy."${r}_cpu" = {
+              name               = "${cfg.clusterName}-${name}-cpu";
+              policy_type        = "TargetTrackingScaling";
+              resource_id        = "\${aws_appautoscaling_target.${r}.resource_id}";
+              scalable_dimension = "\${aws_appautoscaling_target.${r}.scalable_dimension}";
+              service_namespace  = "\${aws_appautoscaling_target.${r}.service_namespace}";
+              target_tracking_scaling_policy_configuration = [{
+                target_value = c.scaling.cpuTarget;
+                predefined_metric_specification = [{ predefined_metric_type = "ECSServiceAverageCPUUtilization"; }];
+              }];
+            };
+          }
+
+          (optionalAttrs (c.scaling.memTarget != null) {
+            resource.aws_appautoscaling_policy."${r}_mem" = {
+              name               = "${cfg.clusterName}-${name}-mem";
+              policy_type        = "TargetTrackingScaling";
+              resource_id        = "\${aws_appautoscaling_target.${r}.resource_id}";
+              scalable_dimension = "\${aws_appautoscaling_target.${r}.scalable_dimension}";
+              service_namespace  = "\${aws_appautoscaling_target.${r}.service_namespace}";
+              target_tracking_scaling_policy_configuration = [{
+                target_value = c.scaling.memTarget;
+                predefined_metric_specification = [{ predefined_metric_type = "ECSServiceAverageMemoryUtilization"; }];
+              }];
+            };
+          })
+
+        ]
+      ) cfg.containers;
+
+    in
+    {
+      terraform.required_providers.aws = {
+        source  = "hashicorp/aws";
+        version = "~> 5.0";
+      };
+
+      provider.aws.region = "eu-west-1";
+
+      networking.vpcs.app = {
+        cidr              = "10.0.0.0/16";
+        availabilityZones = [ "eu-west-1a" "eu-west-1b" ];
+        natGateway        = "single";
+        tags              = { ManagedBy = "terraform"; };
+      };
+
+      services.ecsFargate = {
+        enable      = true;
+        clusterName = "my-app";
+        region      = "eu-west-1";
+        tags        = { ManagedBy = "terraform"; };
+        vpc         = config.networking.vpcs.app.ref;
+
+        containers.api = {
+          image         = "123456789.dkr.ecr.eu-west-1.amazonaws.com/api:latest";
+          cpu           = 512;
+          memory        = 1024;
+          port          = 8080;
+          public        = true;
+          environment   = { NODE_ENV = "production"; };
+          healthCheck   = { path = "/health"; };
+          scaling       = { min = 2; max = 10; cpuTarget = 60; };
+          extraPolicies = [ "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess" ];
+        };
+
+        containers.worker = {
+          image       = "123456789.dkr.ecr.eu-west-1.amazonaws.com/worker:latest";
+          cpu         = 256;
+          memory      = 512;
+          environment.QUEUE_URL = "https://sqs.eu-west-1.amazonaws.com/123/my-queue";
+        };
+      };
+    }
+    // optionalAttrs cfg.enable (
+      foldl' recursiveUpdate {} (
+        [{ resource.aws_ecs_cluster.${cfg.clusterName} = {
+             name = cfg.clusterName;
+             tags = cfg.tags;
+             setting = [{ name = "containerInsights"; value = "enabled"; }];
+           };
+           output.ecs_cluster_name.value = "\${aws_ecs_cluster.${cfg.clusterName}.name}";
+           output.ecs_cluster_arn.value  = "\${aws_ecs_cluster.${cfg.clusterName}.arn}";
+        }]
+        ++ attrValues containerResources
+        ++ mapAttrsToList (name: c:
+             optionalAttrs (c.public && c.port != null) {
+               output."${name}_alb_dns".value = "\${aws_lb.${rn name}.dns_name}";
+             }
+           ) cfg.containers
+      )
+    );
 }
--- modules/nixos/terraform/vpc.nix
+++ modules/nixos/terraform/vpc.nix
@@ -1,76 +1,197 @@
 # terraform/vpc.nix
-{ ... }:
+{ config, lib, ... }:
 
+let
+  inherit (lib)
+    mkOption types mkIf mapAttrs foldl'
+    recursiveUpdate optionalAttrs imap0 attrValues;
+
+  vpcOpts = types.submodule ({ name, config, ... }: {
+    options = {
+      cidr = mkOption {
+        type    = types.str;
+        default = "10.0.0.0/16";
+      };
+
+      availabilityZones = mkOption {
+        type        = types.listOf types.str;
+        description = "One public + one private subnet is created per AZ.";
+        example     = [ "eu-west-1a" "eu-west-1b" ];
+      };
+
+      publicSubnetCidrs = mkOption {
+        type    = types.listOf types.str;
+        description = "Override auto-generated public CIDRs.";
+      };
+
+      privateSubnetCidrs = mkOption {
+        type    = types.listOf types.str;
+        description = "Override auto-generated private CIDRs.";
+      };
+
+      natGateway = mkOption {
+        type    = types.enum [ "single" "perAz" "none" ];
+        default = "single";
+        description = ''
+          single — one NAT GW in AZ 0 (cheaper)
+          perAz  — one NAT GW per AZ (HA)
+          none   — no NAT GW; private subnets have no internet egress
+        '';
+      };
+
+      enableDnsHostnames = mkOption { type = types.bool; default = true; };
+      enableDnsSupport   = mkOption { type = types.bool; default = true; };
+
+      tags = mkOption {
+        type    = types.attrsOf types.str;
+        default = {};
+      };
+
+      ref = mkOption {
+        type     = types.attrs;
+        readOnly = true;
+        description = "Terraform interpolation strings — pass to services.ecsFargate.vpc.";
+      };
+    };
+
+    config = let
+      n    = name;
+      nAzs = builtins.length config.availabilityZones;
+      baseOctet = lib.elemAt (lib.splitString "." config.cidr) 1;
+    in {
+      # Use mkDefault so these can be overridden by the user without recursion
+      publicSubnetCidrs = lib.mkDefault (lib.genList (i: "10.${baseOctet}.${toString i}.0/24") nAzs);
+      privateSubnetCidrs = lib.mkDefault (lib.genList (i: "10.${baseOctet}.${toString (i + 10)}.0/24") nAzs);
+
+      ref = {
+        id = "\${aws_vpc.${n}.id}";
+        privateSubnetIds = lib.genList
+          (i: "\${aws_subnet.${n}_private_${toString i}.id}") nAzs;
+        publicSubnetIds = lib.genList
+          (i: "\${aws_subnet.${n}_public_${toString i}.id}") nAzs;
+      };
+    };
+  });
+
+in
 {
-  resource = {
-    # ── VPC ────────────────────────────────────────────────────────────────
-    aws_vpc.main = {
-      cidr_block           = "10.0.0.0/16";
-      enable_dns_hostnames = true;
-      enable_dns_support   = true;
-      tags.Name            = "main";
-    };
-
-    # ── Internet gateway ───────────────────────────────────────────────────
-    aws_internet_gateway.main = {
-      vpc_id   = "\${aws_vpc.main.id}";
-      tags.Name = "main";
-    };
-
-    # ── Public subnet ──────────────────────────────────────────────────────
-    aws_subnet.public = {
-      vpc_id                  = "\${aws_vpc.main.id}";
-      cidr_block              = "10.0.1.0/24";
-      map_public_ip_on_launch = true;
-      tags.Name               = "public";
-    };
-
-    # ── Route table ────────────────────────────────────────────────────────
-    aws_route_table.public = {
-      vpc_id = "\${aws_vpc.main.id}";
-      route = [{
-        cidr_block = "0.0.0.0/0";
-        gateway_id = "\${aws_internet_gateway.main.id}";
-      }];
-      tags.Name = "public";
-    };
-
-    aws_route_table_association.public = {
-      subnet_id      = "\${aws_subnet.public.id}";
-      route_table_id = "\${aws_route_table.public.id}";
-    };
-
-    # ── Security group ─────────────────────────────────────────────────────
-    aws_security_group.main = {
-      name        = "main";
-      description = "Allow SSH and all egress";
-      vpc_id      = "\${aws_vpc.main.id}";
-
-      ingress = [{
-        description      = "SSH";
-        from_port        = 22;
-        to_port          = 22;
-        protocol         = "tcp";
-        cidr_blocks      = [ "0.0.0.0/0" ];
-        ipv6_cidr_blocks = [];
-        prefix_list_ids  = [];
-        security_groups  = [];
-        self             = false;
-      }];
-
-      egress = [{
-        description      = "All outbound";
-        from_port        = 0;
-        to_port          = 0;
-        protocol         = "-1";
-        cidr_blocks      = [ "0.0.0.0/0" ];
-        ipv6_cidr_blocks = [ "::/0" ];
-        prefix_list_ids  = [];
-        security_groups  = [];
-        self             = false;
-      }];
-
-      tags.Name = "main";
-    };
+  options.networking.vpcs = mkOption {
+    type    = types.attrsOf vpcOpts;
+    default = {};
   };
+
+  config =
+    let
+      vpcResources = mapAttrs (name: vpc:
+        let
+          n      = name;
+          nAzs   = builtins.length vpc.availabilityZones;
+          azList = vpc.availabilityZones;
+
+          natIndices =
+            if vpc.natGateway == "perAz"        then lib.genList (i: i) nAzs
+            else if vpc.natGateway == "single"  then [ 0 ]
+            else [];
+
+        in foldl' recursiveUpdate {} [
+
+          # VPC
+          { resource.aws_vpc.${n} = {
+              cidr_block           = vpc.cidr;
+              enable_dns_hostnames = vpc.enableDnsHostnames;
+              enable_dns_support   = vpc.enableDnsSupport;
+              tags                 = vpc.tags // { Name = n; };
+            };
+          }
+
+          # Internet Gateway
+          { resource.aws_internet_gateway.${n} = {
+              vpc_id = "\${aws_vpc.${n}.id}";
+              tags   = vpc.tags // { Name = "${n}-igw"; };
+            };
+          }
+
+          # Public subnets
+          (foldl' recursiveUpdate {} (imap0 (i: az: {
+            resource.aws_subnet."${n}_public_${toString i}" = {
+              vpc_id                  = "\${aws_vpc.${n}.id}";
+              cidr_block              = builtins.elemAt vpc.publicSubnetCidrs i;
+              availability_zone       = az;
+              map_public_ip_on_launch = true;
+              tags = vpc.tags // { Name = "${n}-public-${toString i}"; Tier = "public"; };
+            };
+          }) azList))
+
+          # Private subnets
+          (foldl' recursiveUpdate {} (imap0 (i: az: {
+            resource.aws_subnet."${n}_private_${toString i}" = {
+              vpc_id            = "\${aws_vpc.${n}.id}";
+              cidr_block        = builtins.elemAt vpc.privateSubnetCidrs i;
+              availability_zone = az;
+              tags = vpc.tags // { Name = "${n}-private-${toString i}"; Tier = "private"; };
+            };
+          }) azList))
+
+          # EIPs + NAT Gateways
+          (foldl' recursiveUpdate {} (map (i: {
+            resource.aws_eip."${n}_nat_${toString i}" = {
+              domain     = "vpc";
+              depends_on = [ "aws_internet_gateway.${n}" ];
+              tags       = vpc.tags // { Name = "${n}-nat-eip-${toString i}"; };
+            };
+            resource.aws_nat_gateway."${n}_${toString i}" = {
+              allocation_id = "\${aws_eip.${n}_nat_${toString i}.id}";
+              subnet_id     = "\${aws_subnet.${n}_public_${toString i}.id}";
+              depends_on    = [ "aws_internet_gateway.${n}" ];
+              tags          = vpc.tags // { Name = "${n}-nat-${toString i}"; };
+            };
+          }) natIndices))
+
+          # Public route table
+          { resource.aws_route_table."${n}_public" = {
+              vpc_id = "\${aws_vpc.${n}.id}";
+              tags   = vpc.tags // { Name = "${n}-public-rt"; };
+              route  = [{ cidr_block = "0.0.0.0/0"; gateway_id = "\${aws_internet_gateway.${n}.id}"; }];
+            };
+          }
+          (foldl' recursiveUpdate {} (imap0 (i: _: {
+            resource.aws_route_table_association."${n}_public_${toString i}" = {
+              subnet_id      = "\${aws_subnet.${n}_public_${toString i}.id}";
+              route_table_id = "\${aws_route_table.${n}_public.id}";
+            };
+          }) azList))
+
+          # Private route tables (one per AZ)
+          (foldl' recursiveUpdate {} (imap0 (i: _:
+            let
+              natIdx = if vpc.natGateway == "perAz" then i else 0;
+              hasNat = vpc.natGateway != "none";
+            in {
+              resource.aws_route_table."${n}_private_${toString i}" = {
+                vpc_id = "\${aws_vpc.${n}.id}";
+                tags   = vpc.tags // { Name = "${n}-private-rt-${toString i}"; };
+              } // optionalAttrs hasNat {
+                route = [{ cidr_block = "0.0.0.0/0"; nat_gateway_id = "\${aws_nat_gateway.${n}_${toString natIdx}.id}"; }];
+              };
+              resource.aws_route_table_association."${n}_private_${toString i}" = {
+                subnet_id      = "\${aws_subnet.${n}_private_${toString i}.id}";
+                route_table_id = "\${aws_route_table.${n}_private_${toString i}.id}";
+              };
+            }
+          ) azList))
+
+          # Outputs
+          {
+            output."${n}_vpc_id".value             = "\${aws_vpc.${n}.id}";
+            output."${n}_public_subnet_ids".value  =
+              lib.genList (i: "\${aws_subnet.${n}_public_${toString i}.id}") nAzs;
+            output."${n}_private_subnet_ids".value =
+              lib.genList (i: "\${aws_subnet.${n}_private_${toString i}.id}") nAzs;
+          }
+
+        ]
+      ) config.networking.vpcs;
+
+    in
+    foldl' recursiveUpdate {} (attrValues vpcResources);
 }
