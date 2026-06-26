24a5b203ec0f031a90c6d9a310d9abc94648f1ed
Sean Cairns <sean.cairnsst@gmail.com>
2026-06-26 04:35:30 +0100

    fix: format nixtest.nix

--- modules/flake-parts/nixtest.nix
+++ modules/flake-parts/nixtest.nix
@@ -1,31 +1,37 @@
 { config, lib, inputs, ... }: {
-  perSystem = { pkgs, system, ... }: let
-    ntlib = import "${inputs.nixtest}/lib" { inherit pkgs; };
-    testDir = "${inputs.self}/tests";
+  perSystem = { pkgs, system, ... }:
+    let
+      ntlib = import "${inputs.nixtest}/lib" { inherit pkgs; };
+      testDir = "${inputs.self}/tests";
 
-    testFiles = let
-      dirContents = builtins.tryEval (builtins.readDir testDir);
+      testFiles =
+        let
+          dirContents = builtins.tryEval (builtins.readDir testDir);
+        in
+        if dirContents.success then
+          builtins.filter (n: builtins.match ".*_test\\.nix" n != null)
+            (builtins.attrNames dirContents.value)
+        else [ ];
+
+      allSuites = builtins.foldl'
+        (acc: f:
+          let
+            mod = import "${testDir}/${f}" { inherit pkgs lib; };
+          in
+          acc // (mod.suites or { })
+        )
+        { }
+        testFiles;
+
+      testCfg = ntlib.mkNixtestConfig {
+        modules = [{ suites = allSuites; }];
+        args = { inherit pkgs ntlib; };
+      };
     in
-      if dirContents.success then
-        builtins.filter (n: builtins.match ".*_test\\.nix" n != null)
-          (builtins.attrNames dirContents.value)
-      else [ ];
-
-    allSuites = builtins.foldl' (acc: f:
-      let
-        mod = import "${testDir}/${f}" { inherit pkgs lib; };
-      in
-      acc // (mod.suites or { })
-    ) { } testFiles;
-
-    testCfg = ntlib.mkNixtestConfig {
-      modules = [{ suites = allSuites; }];
-      args = { inherit pkgs ntlib; };
+    {
+      packages = lib.optionalAttrs (testFiles != [ ]) {
+        nixtests = testCfg.finalConfigJson;
+        nixtests-run = testCfg.app;
+      };
     };
-  in {
-    packages = lib.optionalAttrs (testFiles != [ ]) {
-      nixtests = testCfg.finalConfigJson;
-      nixtests-run = testCfg.app;
-    };
-  };
 }
