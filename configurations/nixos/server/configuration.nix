befeb044e11ecc0dd5f6876a789194dfb27282ba
Sean Cairns <sean.cairnsst@gmail.com>
2026-06-29 01:50:35 +0100

    server: switch to grub+efiInstallAsRemovable (systemd-boot needs mounted /boot)

--- configurations/nixos/server/configuration.nix
+++ configurations/nixos/server/configuration.nix
@@ -12,8 +12,10 @@
     ];
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.devices = [ "nodev" ];
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
 
   networking.hostName = "server"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
