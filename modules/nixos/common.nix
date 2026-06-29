6025b5541eda42b62e616e3b9bcbbf6273b93859
Sean Cairns <sean.cairnsst@gmail.com>
2026-06-29 19:07:55 +0100

    secrets: update desktop SSH key comment from root@desktop to seanc@desktop

--- modules/nixos/common.nix
+++ modules/nixos/common.nix
@@ -14,7 +14,7 @@ let
   systemsKeys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIETE96NnwPAZ0n5y6XcCzoErkrAhulUht/Hho0V829Qy root@laptop" # laptop
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIZ1mor/quSbisUKJivp3lMDvMcQR4wojD/l5Etg6KCV root@server" # server
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWiP0JxNaeWS30gzg4A2zLnSRdZutWzCP0mjZit7/De root@desktop" # desktop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKWiP0JxNaeWS30gzg4A2zLnSRdZutWzCP0mjZit7/De seanc@desktop" # desktop
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKekVg/4uXAmOcRzxbaPn9zW5NTB6te+F0PUXO1FmrkQ seanc@laptop" # nixos-deploy/desktop
   ];
   usersKeys = [ flake.config.me.sshKey ];
